ee6742ec9d760fdccd458b5822dc5021415e152f
saucersauros <733434@student.dusd.net>
2025-11-12 15:26:45 -0800

    Player and hitboxes
    I updated the player movement. i added hitboxes to objects and tiles. i need to fix s the tiles part though.

--- P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -6,21 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
     }
 
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 15.0f * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = (Vector2)rigidbody2d.position + move * 15.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
