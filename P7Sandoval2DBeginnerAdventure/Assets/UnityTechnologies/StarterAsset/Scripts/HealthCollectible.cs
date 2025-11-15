9e441ca432128ac3ef4859df567ec8e6950994a6
saucersauros <733434@student.dusd.net>
2025-11-14 21:10:49 -0800

    Collectibe
    I finsihed with the collectible and is now starting with how the spikes damage

--- P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/HealthCollectible.cs
+++ P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/HealthCollectible.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
+
     void OnTriggerEnter2D(Collider2D other)
     {
         PlayerController controller = other.GetComponent<PlayerController>();
 
-        if (controller != null)
+        if (controller != null && controller.health < controller.maxHealth)
         {
             controller.ChangeHealth(1);
             Destroy(gameObject);
         }
     }
-
- 
 }
--- P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
+++ P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/PlayerController.cs
@@ -5,46 +5,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables related to player character movement
+
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
     public float speed = 3.0f;
 
 
-    // Variables related to the health system
     public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
     int currentHealth = 1;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //currentHealth = maxHealth;
-    }
 
-    // Update is called once per frame
+    }
+ 
+
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
     }
 
-
-    // FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
-
-
     public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
-
 }
