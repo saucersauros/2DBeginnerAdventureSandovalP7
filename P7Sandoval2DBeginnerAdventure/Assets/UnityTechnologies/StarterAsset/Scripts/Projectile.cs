ddf68cadc43f13bf0e805502043cf7c9e356e4e1
saucersauros <733434@student.dusd.net>
2025-12-01 14:07:02 -0800

    exporting it

--- P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/Projectile.cs
+++ P7Sandoval2DBeginnerAdventure/Assets/UnityTechnologies/StarterAsset/Scripts/Projectile.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Projectile : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Launch(Vector2 direction, float force)
     {
-        
+        rigidbody2d.AddForce(direction * force);
+    }
+    void OnTriggerEnter(Collider2D other)
+    {
+        Debug.Log("Projectile collision with" + other.gameObject);
+        Destroy(gameObject);
     }
 }
